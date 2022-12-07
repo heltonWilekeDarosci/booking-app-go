a46807ce270996fbadef983039168a43657bcf9f
Helton Wileke Darosci <heltonwd@hotmail.com>
2022-12-07 17:04:49 -0300

    feat(main): scanning users answers

--- main.go
+++ main.go
@@ -3,12 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here!")
 
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("What's your first name? ")
+	fmt.Scan(&firstName)
+	fmt.Println("What's your last name? ")
+	fmt.Scan(&lastName)
+	fmt.Println("What's your email? ")
+	fmt.Scan(&email)
+	fmt.Println("How many tickets do you want to purchase? ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You'll receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 }
